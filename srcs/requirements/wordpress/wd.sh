0a64d5e6f946d7bec4ad3ab6e9c66d339a62fcd0
cchadili <chadilisafi@gmail.com>
2025-09-06 07:48:06 -0400

    mandatory part almost complet

--- srcs/requirements/wordpress/wd.sh
+++ srcs/requirements/wordpress/wd.sh
@@ -1,52 +1,17 @@
 #!/bin/bash
-# set -e
+sleep 5
+cd /var/www/html
 
-# # Wait for MariaDB to be ready
-# echo "Waiting for MariaDB..."
-# until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
-#   sleep 2
-# done
-# echo "MariaDB is up!"
-
-# # Generate wp-config.php if it doesn’t exist
-# if [ ! -f wp-config.php ]; then
-#   wp config create \
-#     --dbname=$WORDPRESS_DB_NAME \
-#     --dbuser=$WORDPRESS_DB_USER \
-#     --dbpass=$WORDPRESS_DB_PASSWORD \
-#     --dbhost=$WORDPRESS_DB_HOST \
-#     --allow-root
-# fi
-
-# # Install WordPress if not installed
-# if ! wp core is-installed --allow-root; then
-#   wp core install \
-#     --url=$URL \
-#     --title=$TITLE \
-#     --admin_user=$ADMINUSER \
-#     --admin_password=$ADMINPASSWORD \
-#     --admin_email=$EMAILADMINE \
-#     --skip-email \
-#     --allow-root
-# fi
-
-# exec php-fpm8.2 -F
-
-# mkdir -p /run/php
-# sleep 10
-# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# chmod +x wp-cli.phar
-# mv wp-cli.phar /usr/local/bin/wp
-# wp core download --allow-root
-# sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
-# wp config create --dbname=${NAME_DATABASE} --dbuser=${NAME_USER} --dbpass=${USER_PASSWORD} --dbhost=${DB_HOST} --allow-root
-  # wp config create \
-  #   --dbname=$WORDPRESS_DB_NAME \
-  #   --dbuser=$WORDPRESS_DB_USER \
-  #   --dbpass=$WORDPRESS_DB_PASSWORD \
-  #   --dbhost=$WORDPRESS_DB_HOST \
-  #   --allow-root --path='/var/www/html'
-
-# wp core install --url=${URL} --title=${TITLE} --admin_user=${ADMINUSER} --admin_password=${ADMINPASSWORD} --admin_email=${EMAILADMINE} --allow-root
-# wp user create ${USERNAME} ${EMAILUSR} --role=${ROLE} --user_pass=${USERPASSWORD} --allow-root 
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+wp core download --allow-root
+wp config create \
+    --dbname=$WORDPRESS_DB_NAME \
+    --dbuser=$WORDPRESS_DB_USER \
+    --dbpass=$WORDPRESS_DB_PASSWORD \
+    --dbhost=$WORDPRESS_DB_HOST \
+    --allow-root --path='/var/www/html'
+wp core install --url=${URL} --title=${TITLE} --admin_user=${ADMINUSER} --admin_password=${ADMINPASSWORD} --admin_email=${EMAILADMINE} --allow-root
+wp user create ${USERNAME} ${EMAILUSR} --role=${ROLE} --user_pass=${USERPASSWORD} --allow-root 
 exec php-fpm8.2 -F
