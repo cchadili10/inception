ca01eadb019f0c3eacd4c6362a91e3071a8255d8
cchadili <chadilisafi@gmail.com>
2025-09-24 07:09:15 -0400

    modfy some files

---
+++ srcs/requirements/nginx/tools/spt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /etc/nginx/ssl
+
+openssl req -x509 -nodes -out /etc/nginx/ssl/inception.crt -keyout /etc/nginx/ssl/inception.key -subj "/C=MA/ST=KHO/L=khouribga/O=42/OU=42/CN=hchadili.42.fr/UID=hchadili"
+
+
+nginx -g "daemon off;" 
